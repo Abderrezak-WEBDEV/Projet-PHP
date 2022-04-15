f6e88830237ba6c1e9cd4eb105b50b4e535ca640
Guillaume PAGLIACCIA <pagliaccia.boivin.g@gmail.com>
2022-04-15 16:41:38 +0200

    commit 16h41

--- inscriptionbdd.php
+++ inscriptionbdd.php
@@ -6,7 +6,7 @@ $Mail = (string)htmlspecialchars($_POST['Mail']);
 $Motdepasse = (string)htmlspecialchars($_POST['Motdepasse']);
 $Pseudo = (string)htmlspecialchars($_POST['Pseudo']);
 
-$dbh = new PDO("mysql:dbname=enchere;host=localhost", "root", "root");
+$dbh = new PDO("mysql:dbname=enchere;host=localhost", "root", "");
 $query = $dbh->prepare("INSERT INTO Users (firstname, lastname, email, pw, pseudo) VALUES (?,?,?,?,?);");
 $result = $query->execute([$Nom, $Prenom, $Mail, $Motdepasse, $Pseudo]);
 
