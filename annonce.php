3409f7f1782f6e9561a80e6fb113db31ef067a16
Guillaume PAGLIACCIA <pagliaccia.boivin.g@gmail.com>
2022-04-20 14:57:33 +0200

    boncommit

--- annonce.php
+++ annonce.php
@@ -3,7 +3,7 @@ include __DIR__ . "/dbh.php";
 include_once __DIR__ . "/header.php";
 include_once __DIR__ . "/detail_enchere.php";
 ?>
-<?php $dbh = new PDO("mysql:dbname=carenchere;host=127.0.0.1", "root", ""); ?>
+<?php $dbh = new PDO("mysql:dbname=enchere;host=127.0.0.1", "root", ""); ?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -22,28 +22,28 @@ include_once __DIR__ . "/detail_enchere.php";
 		$vehicule = $query->fetchAll(PDO::FETCH_ASSOC); ?>
 
 		<?php foreach ($vehicule as $vehicule) { ?>
-		<h1>Vendeur - <?= $vehicule["pseudo"] ?> </h1>
-		<p>Id du vendeur : <?= $vehicule["id_user"] ?></p>
-		<p>Prix de départ : <?= $vehicule["prixdepart"] ?></p>
-		<p>Fin de l'encher : <?= $vehicule["enddate"] ?></p>
-		<p>Model du véhicule : <?= $vehicule["model"] ?></p>
-		<p>Marque du véhicule : <?= $vehicule["marque"] ?></p>
-		<p>Année du véhicule : <?= $vehicule["yearmodel"] ?></p>
-		<p>Puissance : <?= $vehicule["power"] ?></p>
-		<p>Description : <?= $vehicule["description"] ?></p>
-		<p>Total enchére du véhicule : <?= $vehicule["totalenchere"] ?></p>
-		<p>Id de l'annonce :<?= $vehicule["id"] ?></p><br>
+			<h1>Vendeur - <?= $vehicule["pseudo"] ?> </h1>
+			<p>Id du vendeur : <?= $vehicule["id_user"] ?></p>
+			<p>Prix de départ : <?= $vehicule["prixdepart"] ?></p>
+			<p>Fin de l'encher : <?= $vehicule["enddate"] ?></p>
+			<p>Model du véhicule : <?= $vehicule["model"] ?></p>
+			<p>Marque du véhicule : <?= $vehicule["marque"] ?></p>
+			<p>Année du véhicule : <?= $vehicule["yearmodel"] ?></p>
+			<p>Puissance : <?= $vehicule["power"] ?></p>
+			<p>Description : <?= $vehicule["description"] ?></p>
+			<p>Total enchére du véhicule : <?= $vehicule["totalenchere"] ?></p>
+			<p>Id de l'annonce :<?= $vehicule["id"] ?></p><br>
 
-		<?php if (new DateTime($vehicule["enddate"]) > new DateTime()) { ?>
-		<form action="annonce.php" method="POST">
-			<input type="hidden" name="id_annonce" value="<?= $vehicule["id"]; ?>" placeholder="-">
-			<input type="number" name="price" />
-			<input type="submit" value="Valider">
-		</form>
-		<?php } else { ?>
-		<p>Enchères terminées</p>
-		<?php } ?>
-		<hr>
+			<?php if (new DateTime($vehicule["enddate"]) > new DateTime()) { ?>
+				<form action="annonce.php" method="POST">
+					<input type="hidden" name="id_annonce" value="<?= $vehicule["id"]; ?>" placeholder="-">
+					<input type="number" name="price" />
+					<input type="submit" value="Valider">
+				</form>
+			<?php } else { ?>
+				<p>Enchères terminées</p>
+			<?php } ?>
+			<hr>
 		<?php } ?>
 
 
@@ -62,69 +62,69 @@ include_once __DIR__ . "/detail_enchere.php";
 		<br>
 	</div>
 	<style>
-	h1 {
-		color: rgb(0, 255, 136);
-		text-align: center;
-	}
+		h1 {
+			color: rgb(0, 255, 136);
+			text-align: center;
+		}
 
-	p {
-		margin-left: 20px;
-	}
+		p {
+			margin-left: 20px;
+		}
 
-	#annonces {
-		background-color: white;
-		box-shadow: 10px 5px 5px rgb(0, 255, 136);
-		width: 50%;
-		margin-left: 25%;
-	}
+		#annonces {
+			background-color: white;
+			box-shadow: 10px 5px 5px rgb(0, 255, 136);
+			width: 50%;
+			margin-left: 25%;
+		}
 
-	button {
-		width: 80%;
-		margin-left: 10%;
-		height: 30px;
-		border-radius: 5px;
-	}
+		button {
+			width: 80%;
+			margin-left: 10%;
+			height: 30px;
+			border-radius: 5px;
+		}
 
-	input {
-		background-color: rgb(0, 255, 136);
-	}
+		input {
+			background-color: rgb(0, 255, 136);
+		}
 
-	#enchere {
-		display: flex;
-		flex-direction: column;
-		gap: 20px;
-		margin-bottom: 10px;
-	}
-
-
+		#enchere {
+			display: flex;
+			flex-direction: column;
+			gap: 20px;
+			margin-bottom: 10px;
+		}
 
 
-	<style>h1 {
-		color: rgb(0, 255, 136);
-		text-align: center;
-	}
 
-	p {
-		margin-left: 20px;
-	}
 
-	#annonces {
-		background-color: white;
-		box-shadow: 10px 5px 5px rgb(0, 255, 136);
-		width: 50%;
-		margin-left: 25%;
-	}
+		<style>h1 {
+			color: rgb(0, 255, 136);
+			text-align: center;
+		}
 
-	button {
-		width: 80%;
-		margin-left: 10%;
-		height: 30px;
-		border-radius: 5px;
-	}
+		p {
+			margin-left: 20px;
+		}
 
-	input {
-		background-color: rgb(0, 255, 136);
-	}
+		#annonces {
+			background-color: white;
+			box-shadow: 10px 5px 5px rgb(0, 255, 136);
+			width: 50%;
+			margin-left: 25%;
+		}
+
+		button {
+			width: 80%;
+			margin-left: 10%;
+			height: 30px;
+			border-radius: 5px;
+		}
+
+		input {
+			background-color: rgb(0, 255, 136);
+		}
 	</style>
 </body>
 
